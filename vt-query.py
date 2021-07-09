1a4b8680cf045340c646236f0037cfbe2f9d24f9
Kameryn Taylor <taylorkameryn26@gmail.com>
2021-07-09 04:51:33 -0700

    <returns analysis id>

--- vt-query.py
+++ vt-query.py
@@ -1,1 +1,10 @@
-print ("Hello World")
+import sys
+import os
+import json
+import requests
+import config as cfg
+
+
+print (cfg.post_url)
+result = requests.post(cfg.post_url, headers = {"x-apikey": cfg.my_key}, data = {"url": cfg.query_url})
+print(result.text)
