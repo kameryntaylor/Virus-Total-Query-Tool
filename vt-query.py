30cbae63d5beea5b5945f28abcdec730d4ea54d2
Kameryn Taylor <taylorkameryn26@gmail.com>
2021-07-17 20:01:47 -0700

    update

--- vt-query.py
+++ vt-query.py
@@ -4,7 +4,13 @@ import json
 import requests
 import config as cfg
 
-
-print (cfg.post_url)
-result = requests.post(cfg.post_url, headers = {"x-apikey": cfg.my_key}, data = {"url": cfg.query_url})
-print(result.text)
+#URL
+analysis_id = requests.post(cfg.base_url, headers = {"x-apikey": cfg.my_key}, data = {"url": cfg.query_url}).json()
+print (analysis_id)
+#URL{ID}
+only_id = (analysis_id.get('data').get('id'))[2:]
+#URL{ID}/ANALYSE
+build_url = cfg.base_url + "/" + only_id + "/analyse"
+happening = requests.post(cfg.base_url, headers = {"x-apikey": cfg.my_key}, data = {"url": build_url})
+print(happening.text)
+#ANALYSES/ID
